69943a76def278dfc3d33cc1e9d50d7773847840
Daniel Penz <daniel.penz@gmx.de>
2018-05-29 15:55:52 +0200

    feat: Implement AsyncTask to access the Internet

--- app/src/main/java/com/example/surface4pro/movielicious/MainActivity.java
+++ app/src/main/java/com/example/surface4pro/movielicious/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.surface4pro.movielicious;
 
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import com.example.surface4pro.movielicious.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +35,38 @@ public class MainActivity extends AppCompatActivity {
                 .appendQueryParameter(PARAM_QUERY, API_KEY)
                 .build();
         Log.d("URI: ", builtUri.toString());
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        new FetchMoviesTask().execute(url);
+
+    }
+
+    public class FetchMoviesTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL queryUrl = urls[0];
+            String movieQueryResults = null;
+            try {
+                movieQueryResults = NetworkUtils.getResponseFromHttpUrl(queryUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+            return movieQueryResults;
+        }
+
+        @Override
+        protected void onPostExecute(String movieQueryResults) {
+            if (movieQueryResults != null && !movieQueryResults.equals("")) {
+                Log.d("HTTP Result: ", movieQueryResults);
+            }
+        }
     }
 }
