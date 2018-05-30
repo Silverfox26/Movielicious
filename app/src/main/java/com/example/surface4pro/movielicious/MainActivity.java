7c5c75da37be1f8f9fccb8cdc8aa3dd916f11b79
Daniel Penz <daniel.penz@gmx.de>
2018-05-30 12:28:14 +0200

    refactor: Refactor buildURL method to NetworkUtils class

--- app/src/main/java/com/example/surface4pro/movielicious/MainActivity.java
+++ app/src/main/java/com/example/surface4pro/movielicious/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.surface4pro.movielicious;
 
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,39 +8,17 @@ import android.util.Log;
 import com.example.surface4pro.movielicious.utilities.NetworkUtils;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Defining URL constants
-    final static String MOVIEDB_BASE_URL = "http://api.themoviedb.org/3";
-    final static String PATH_MOVIES = "movie";
-    final static String PATH_SORT_POPULAR = "popular";
-    final static String PATH_SORT_TOP_RATED = "top_rated";
-    final static String PARAM_QUERY = "api_key";
-    final static String API_KEY = "YOUR_API_KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* Building the API request URL */
-
-        Uri builtUri = Uri.parse(MOVIEDB_BASE_URL).buildUpon()
-                .appendPath(PATH_MOVIES)
-                .appendPath(PATH_SORT_POPULAR)
-                .appendQueryParameter(PARAM_QUERY, API_KEY)
-                .build();
-        Log.d("URI: ", builtUri.toString());
-
-        URL url = null;
-        try {
-            url = new URL(builtUri.toString());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+        URL url = NetworkUtils.buildURL();
 
         new FetchMoviesTask().execute(url);
 
--- app/src/main/java/com/example/surface4pro/movielicious/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/surface4pro/movielicious/utilities/NetworkUtils.java
@@ -1,8 +1,12 @@
 package com.example.surface4pro.movielicious.utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -11,6 +15,33 @@ import java.util.Scanner;
  */
 public final class NetworkUtils {
 
+    // Defining URL constants
+    final static String MOVIEDB_BASE_URL = "http://api.themoviedb.org/3";
+    final static String PATH_MOVIES = "movie";
+    final static String PATH_SORT_POPULAR = "popular";
+    final static String PATH_SORT_TOP_RATED = "top_rated";
+    final static String PARAM_QUERY = "api_key";
+    final static String API_KEY = "YOUR_API_KEY";
+
+    public static URL buildURL() {
+        /* Building the API request URL */
+
+        Uri builtUri = Uri.parse(MOVIEDB_BASE_URL).buildUpon()
+                .appendPath(PATH_MOVIES)
+                .appendPath(PATH_SORT_POPULAR)
+                .appendQueryParameter(PARAM_QUERY, API_KEY)
+                .build();
+        Log.d("URI: ", builtUri.toString());
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
 
     /**
      * Method returns the entire result from the HTTP response.
